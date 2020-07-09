d5c6c6b9c6891eff633bb01f35d797a471a18e03
Baptiste-Caral <baptiste.caral@gmail.com>
2020-07-09 15:53:43 +0200

    make a 10 seconds timer

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('try');
+const btn = document.querySelector('.btn');
+let secondes = 10;
+let interval;
+
+
+const start = () => {
+  interval = setInterval(decompte, 1000);
+}
+
+btn.addEventListener('click',start);
+
+
+const stop = () => {
+  clearInterval(interval);
+  document.body.innerHTML = 'Terminé !';
+}
+const decompte = () => {
+
+  secondes--;
+  if (secondes == 0) {
+    stop()  
+  }
+  else
+  document.body.innerHTML = secondes;
+}
+
