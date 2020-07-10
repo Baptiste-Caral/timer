1ebd06bae96eff92a273397a5d829c3e9cc86780
Baptiste-Caral <baptiste.caral@gmail.com>
2020-07-11 01:16:50 +0200

    fix first number issue

--- app.js
+++ app.js
@@ -4,57 +4,54 @@ const count = document.querySelector('.counter')
 let secondes;
 let interval;
 
-
 const start = () => {
-
   interval = setInterval(decompte, 1000);
   btn.innerHTML = 'Stop';
-  input.hidden;
+  input.style.display = 'none';
 }
 
-btn.addEventListener('click',(e) => {
-  e.preventDefault();
-  if (isNaN(input.value) && !'...') {
-    
-    alert('Veuillez rentrer un nombre svp, pas de texte')
-
-  } else if (secondes > 0) {
+const decompte = () => { 
+  secondes--;
+  if (secondes <= 0) {
     stop();
-    e.preventDefault();
-    
-  } else {
-
-    secondes = input.value;
-    start();
-    e.preventDefault();
-    input.value = ' ... ';
+    const audio = new Audio('./assets/bip.mp3');
+    audio.play();
   }
-});
-
-
+  else
+  count.innerHTML = secondes;  
+}
 const stop = () => {
 
   const reset = () => {
     count.innerHTML = "0";
+    input.style.display = 'block';
+    btn.innerHTML = 'Lancer le décompte';
   } 
   clearInterval(interval);
   count.innerHTML = "<span style='font-size:40px'>Terminé !</span>";
-  secondes = 0;
-  btn.innerHTML = 'Lancer le décompte';
+  secondes = 0; 
   setTimeout(reset, 1500);
-  input.value = ''
-}
-const decompte = () => {
-
-  secondes--;
-  if (secondes <= 0) {
-    stop();
-    const audio = new Audio('bip.mp3');
-    audio.play();
-    
-  }
-  else
-  count.innerHTML = secondes;
+  input.value = '';
   
 }
+btn.addEventListener('click',(e) => {
+
+  e.preventDefault();
+
+  if (isNaN(input.value)) { 
+    alert('Veuillez rentrer un nombre svp, pas de texte')
+  } else if (secondes > 0) {
+    stop();
+    e.preventDefault(); 
+  } else {
+    secondes = input.value;
+    count.innerHTML = secondes;
+    start();
+    e.preventDefault();
+  }
+});
+
+
+
+
 
