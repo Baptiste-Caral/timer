cd7f2bfc0b1c6bbc04eb1eeff369633e6a122d01
Baptiste-Caral <baptiste.caral@gmail.com>
2020-07-10 21:17:40 +0200

    change input value

--- app.js
+++ app.js
@@ -8,34 +8,53 @@ let interval;
 const start = () => {
 
   interval = setInterval(decompte, 1000);
+  btn.innerHTML = 'Stop';
+  input.hidden;
 }
 
 btn.addEventListener('click',(e) => {
   e.preventDefault();
-  if (isNaN(input.value)) {
+  if (isNaN(input.value) && !'...') {
     
     alert('Veuillez rentrer un nombre svp, pas de texte')
 
+  } else if (secondes > 0) {
+    stop();
+    e.preventDefault();
+    
   } else {
 
     secondes = input.value;
     start();
     e.preventDefault();
+    input.value = ' ... ';
   }
 });
 
 
 const stop = () => {
+
+  const reset = () => {
+    count.innerHTML = "0";
+  } 
   clearInterval(interval);
-  count.innerHTML = 'Terminé !';
+  count.innerHTML = "<span style='font-size:40px'>Terminé !</span>";
+  secondes = 0;
+  btn.innerHTML = 'Lancer le décompte';
+  setTimeout(reset, 1500);
+  input.value = ''
 }
 const decompte = () => {
 
   secondes--;
   if (secondes <= 0) {
-    stop()  
+    stop();
+    const audio = new Audio('bip.mp3');
+    audio.play();
+    
   }
   else
   count.innerHTML = secondes;
+  
 }
 
